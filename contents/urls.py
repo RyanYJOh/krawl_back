77e4efa6e91bb4f65b8eff3d88b93d3876442407
RyanYJOh <hiimryan@kakao.com>
2022-03-14 10:46:04 +0900

    slash

--- contents/urls.py
+++ contents/urls.py
@@ -5,13 +5,13 @@ from django.conf.urls import include
 urlpatterns = [
     # path('api', views.apiOverview),
     path('', views.getAllPosts),
-    path('post-content/', views.post),
-    path('get-post/<int:pk>/', views.getThisPost),
-    path('get-popular-posts/', views.getPopular),
-    path('likes/', views.postLike), 
-    path('likes/<int:pk>/', views.getDelLike),
-    path('post-comment/', views.postComment),
-    path('get-comment/<int:pk>/', views.getComments),
-    path('del-comment/<int:pk>/', views.delComment),
+    path('post-content', views.post),
+    path('get-post/<int:pk>', views.getThisPost),
+    path('get-popular-posts', views.getPopular),
+    path('likes', views.postLike), 
+    path('likes/<int:pk>', views.getDelLike),
+    path('post-comment', views.postComment),
+    path('get-comment/<int:pk>', views.getComments),
+    path('del-comment/<int:pk>', views.delComment),
     path('auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
